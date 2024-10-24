bf0fcf58a9111452ffb23e6f48f2bd503abd33a5
MarianaToledo <howdypartner0v0@gmail.com>
2024-10-24 19:00:15 -0300

    figuring things out

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,19 +1,25 @@
-const darkModeToggle = document.querySelector(".dark-mode");
-const body = document.body;
+document.addEventListener("DOMContentLoaded", function() {
+    const darkModeToggle = document.querySelector(".dark-mode");
+    if (darkModeToggle) {
+        const body = document.body;
 
+        // Verificar si el modo oscuro estaba habilitado antes
+        if (localStorage.getItem('darkMode') === 'enabled') {
+            body.classList.add('active');
+        }
 
-if (localStorage.getItem('darkMode') === 'enabled') {
-    body.classList.add('active');
-}
+        darkModeToggle.addEventListener("click", () => {
+            console.log("Dark mode toggled");
+            body.classList.toggle("active");
 
-darkModeToggle.addEventListener("click", () => {
-    console.log("Dark mode toggled"); 
-    body.classList.toggle("active");
-
-    // Guardar el estado en localStorage
-    if (body.classList.contains('active')) {
-        localStorage.setItem('darkMode', 'enabled');
+            // Guardar el estado en localStorage
+            if (body.classList.contains('active')) {
+                localStorage.setItem('darkMode', 'enabled');
+            } else {
+                localStorage.setItem('darkMode', 'disabled');
+            }
+        });
     } else {
-        localStorage.setItem('darkMode', 'disabled');
+        console.error("No se encontró el elemento con la clase 'dark-mode'");
     }
 });
